ad49d85886bdca5c6b7c45d7dd60f0cfdd438f91
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-12 16:45:56 +0100

    Creation card widget pour afficher les posts de l'utilisateur dans la page de profin

---
+++ lib/widget.dart/mini_card_widget.dart
@@ -0,0 +1,27 @@
+import 'package:avis/widget.dart/image_post_content_widget.dart';
+import 'package:avis/widget.dart/post_reactions_widget.dart';
+import 'package:flutter/material.dart';
+
+class MiniCardWidget extends StatelessWidget {
+  const MiniCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(24),
+          side: BorderSide(color: Colors.white, width: 2),
+        ),
+        child: Stack(
+          alignment: AlignmentGeometry.bottomCenter,
+          children: [
+            const ImagePostContentWidget(heights: 200),
+            const PostReactionsWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}

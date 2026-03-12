8cf9ca4379716980ffd6cb251c5b80ded56c57e9
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-12 16:50:50 +0100

    Ajout de la nouvelle widget d'avatar de l'utilisateur

--- lib/widget.dart/header_card_widget.dart
+++ lib/widget.dart/header_card_widget.dart
@@ -1,5 +1,6 @@
 import 'package:avis/widget.dart/corps_text_widget.dart';
 import 'package:avis/widget.dart/titre_text_widget.dart';
+import 'package:avis/widget.dart/user_avatar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -12,7 +13,7 @@ class HeaderCardWidget extends StatelessWidget {
       padding: const EdgeInsets.all(12.0),
       child: Row(
         children: [
-          const CircleAvatar(radius: 24, child: TitreTextWidget(texte: "S")),
+          const UserAvatarWidget(radius: 24),
           const Gap(8),
           const Column(
             crossAxisAlignment: CrossAxisAlignment.start,

e5aaeaf2ff64be8c20aaa8129c26a5998fd98255
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-12 15:35:04 +0100

    Ajout ecran de creation d'un nouveau post

---
+++ lib/Screens/new_post_screen.dart
@@ -0,0 +1,45 @@
+import 'package:avis/widget.dart/photos_post_form_widget.dart';
+import 'package:avis/widget.dart/tab_bar_buton_widget.dart';
+import 'package:avis/widget.dart/texte_post_form_widget.dart';
+import 'package:avis/widget.dart/titre_text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NewPostScreen extends StatelessWidget {
+  const NewPostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(title: TitreTextWidget(texte: "Nouveau post")),
+        body: Column(
+          children: [
+            TabBar(
+              tabs: [
+                Tab(
+                  child: TabBarButonWidget(
+                    icone: const FaIcon(FontAwesomeIcons.image),
+                    texte: "Photos",
+                  ),
+                ),
+                const Tab(
+                  child: TabBarButonWidget(
+                    icone: FaIcon(FontAwesomeIcons.lightbulb),
+                    texte: "Idées",
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: const TabBarView(
+                children: [PhotosPostFormWidget(), TextePostFormWidget()],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

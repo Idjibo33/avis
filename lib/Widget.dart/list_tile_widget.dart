46aabbe497571ef1eb5e9005589949fa66db69e5
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-13 21:10:30 +0100

    AJout d'argument au widget

--- lib/Widget.dart/list_tile_widget.dart
+++ lib/Widget.dart/list_tile_widget.dart
@@ -2,14 +2,21 @@ import 'package:avis/Widget.dart/titre_text_widget.dart';
 import 'package:flutter/material.dart';
 
 class ListTileWidget extends StatelessWidget {
+  final VoidCallback? action;
   final Icon? leading;
   final String titre;
-  const ListTileWidget({super.key, required this.titre, this.leading});
+  const ListTileWidget({
+    super.key,
+    required this.titre,
+    this.leading,
+    this.action,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: action,
         leading: leading,
         title: TitreTextWidget(texte: titre),
       ),

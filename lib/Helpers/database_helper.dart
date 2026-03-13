098cd7e8a22801620788525d417159cdf927e18a
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-13 20:53:57 +0100

    Ajout de parametre à la fonction 'readDataOnce'

--- lib/Helpers/database_helper.dart
+++ lib/Helpers/database_helper.dart
@@ -2,6 +2,6 @@ abstract class DatabaseHelper<T> {
   Future addData(T item);
   Future updateData(T item);
   Future deletedata(T item);
-  Future readDataOnce();
+  Future readDataOnce(String id);
   Stream<List<T>> readData();
 }

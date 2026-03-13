50bd1808146ca2ca125bd542b60d80a7c7b79711
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-13 21:07:22 +0100

    Ajout d'argument au widget

--- lib/Widget.dart/bouton_secondaire_widget.dart
+++ lib/Widget.dart/bouton_secondaire_widget.dart
@@ -2,18 +2,26 @@ import 'package:avis/Widget.dart/titre_text_widget.dart';
 import 'package:flutter/material.dart';
 
 class BoutonSecondaireWidget extends StatelessWidget {
+  final bool chargement;
+  final VoidCallback action;
   final String texte;
   final Icon? icone;
-  const BoutonSecondaireWidget({super.key, this.icone, required this.texte});
+  const BoutonSecondaireWidget({
+    super.key,
+    this.icone,
+    required this.texte,
+    required this.action,
+    required this.chargement,
+  });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 45,
       child: ElevatedButton.icon(
-        onPressed: () {},
+        onPressed: chargement ? null : action,
         label: TitreTextWidget(texte: texte),
-        icon: icone,
+        icon: chargement ? CircularProgressIndicator.adaptive() : icone,
       ),
     );
   }

462ca0b74c1eb0fe80534f9740c8f58d61e49848
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-13 21:14:19 +0100

    Ajout fonction d'authentification

--- lib/Widget.dart/drawer_widget.dart
+++ lib/Widget.dart/drawer_widget.dart
@@ -1,8 +1,11 @@
+import 'package:avis/Helpers/navigators.dart';
+import 'package:avis/Providers/auth_services_providers.dart';
 import 'package:avis/Widget.dart/list_tile_widget.dart';
 import 'package:avis/Widget.dart/logo.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
+import 'package:provider/provider.dart';
 
 class DrawerWidget extends StatelessWidget {
   const DrawerWidget({super.key});
@@ -12,21 +15,36 @@ class DrawerWidget extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: ListView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Logo(size: 40),
-            const Gap(30),
-            const ListTileWidget(
-              leading: FaIcon(FontAwesomeIcons.user),
-              titre: "Profil",
+            Column(
+              children: [
+                const Logo(size: 40),
+                const Gap(30),
+                const ListTileWidget(
+                  leading: FaIcon(FontAwesomeIcons.user),
+                  titre: "Profil",
+                ),
+                const ListTileWidget(
+                  leading: FaIcon(FontAwesomeIcons.plus),
+                  titre: "Faire un post",
+                ),
+                const ListTileWidget(
+                  leading: FaIcon(FontAwesomeIcons.bell),
+                  titre: "Activités",
+                ),
+              ],
             ),
-            const ListTileWidget(
-              leading: FaIcon(FontAwesomeIcons.plus),
-              titre: "Faire un post",
-            ),
-            const ListTileWidget(
-              leading: FaIcon(FontAwesomeIcons.bell),
-              titre: "Activités",
+            Consumer<AuthServicesProviders>(
+              builder: (context, auth, child) => ListTileWidget(
+                action: () async {
+                  await auth.signOutuser();
+                  if (context.mounted) naviguerAuthGate(context);
+                },
+                leading: FaIcon(FontAwesomeIcons.arrowRightFromBracket),
+                titre: "Déconnecter",
+              ),
             ),
           ],
         ),

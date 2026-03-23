ac3dc0a4dc1e569358ebcf90b7c2ae772caecf9f
Djibo Ismael <idjibo.aa@ilimi.edu.ne>
2026-03-23 23:40:10 +0100

    Affichage des votes dans la page de resultat du duel

--- lib/Screens/resultat_duel_screen.dart
+++ lib/Screens/resultat_duel_screen.dart
@@ -1,13 +1,14 @@
 import 'package:avis/Models/post.dart';
-import 'package:avis/Widget.dart/duel_gagnant_widget.dart';
+import 'package:avis/Providers/interactions_services_provider.dart';
 import 'package:avis/Widget.dart/duel_resultat_pourcentage_widget.dart';
+import 'package:avis/Widget.dart/list_tile_widget.dart';
 import 'package:avis/Widget.dart/resultat_card_widget.dart';
 import 'package:avis/Widget.dart/titre_text_widget.dart';
-import 'package:avis/Widget.dart/vote_description_widget.dart';
 import 'package:avis/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
+import 'package:provider/provider.dart';
 
 class ResultatDuelScreen extends StatelessWidget {
   final Post post;
@@ -17,15 +18,51 @@ class ResultatDuelScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: TitreTextWidget(texte: "Resultat du duel")),
-      body: ListView(
-        children: [
-          Gap(12),
-          TitreTextWidget(texte: post.question, align: TextAlign.center),
-          Gap(12),
-          ResultatCardWidget(post: post),
-          DuelResultatPourcentageWidget(postId: post.id!),
-          Gap(16),
-          DuelGagnantWidget(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const Gap(12),
+                TitreTextWidget(texte: post.question, align: TextAlign.center),
+                const Gap(12),
+                ResultatCardWidget(post: post),
+                DuelResultatPourcentageWidget(postId: post.id!),
+              ],
+            ),
+          ),
+          Consumer<InteractionsServicesProvider>(
+            builder: (context, interactions, child) => FutureBuilder(
+              future: interactions.readInteractions(postId: post.id!),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  if (snapshot.data != null || snapshot.data!.isNotEmpty) {
+                    final vote = snapshot.data;
+                    return SliverList.builder(
+                      itemCount: vote!.length,
+                      itemBuilder: (context, index) => ListTileWidget(
+                        leading: FaIcon(
+                          FontAwesomeIcons.layerGroup,
+                          color: vote[index].choix == "A"
+                              ? couleureBleue
+                              : couleureRose,
+                        ),
+                        titre: vote[index].choix!,
+                      ),
+                    );
+                  }
+                }
+                if (snapshot.hasError) {
+                  return SliverToBoxAdapter(
+                    child: TitreTextWidget(texte: snapshot.error.toString()),
+                  );
+                }
+                return const SliverToBoxAdapter(
+                  child: Center(child: CircularProgressIndicator()),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
--- lib/Widget.dart/mini_card_widget.dart
+++ lib/Widget.dart/mini_card_widget.dart
@@ -1,4 +1,5 @@
 import 'package:avis/Models/post.dart';
+import 'package:avis/Screens/resultat_duel_screen.dart';
 import 'package:avis/Widget.dart/image_post_content_widget.dart';
 import 'package:avis/Widget.dart/post_reactions_widget.dart';
 import 'package:avis/Widget.dart/text_post_content_widget.dart';
@@ -13,27 +14,33 @@ class MiniCardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     bool isTextPost = post.type == 'text';
 
-    return SizedBox(
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              isTextPost
-                  ? TextPostContentWidget(
-                      heights: 150,
-                      optionA: post.option_a_text!,
-                      optionB: post.option_a_text!,
-                    )
-                  : ImagePostContentWidget(
-                      heights: 150,
-                      optionAUrl: post.option_a_url!,
-                      optionBUrl: post.option_b_url!,
-                    ),
-            ],
-          ),
-          const Gap(4),
-          PostReactionsWidget(postId: post.id!),
-        ],
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => ResultatDuelScreen(post: post)),
+      ),
+      child: SizedBox(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                isTextPost
+                    ? TextPostContentWidget(
+                        heights: 150,
+                        optionA: post.option_a_text!,
+                        optionB: post.option_a_text!,
+                      )
+                    : ImagePostContentWidget(
+                        heights: 150,
+                        optionAUrl: post.option_a_url!,
+                        optionBUrl: post.option_b_url!,
+                      ),
+              ],
+            ),
+            const Gap(4),
+            PostReactionsWidget(postId: post.id!),
+          ],
+        ),
       ),
     );
   }
